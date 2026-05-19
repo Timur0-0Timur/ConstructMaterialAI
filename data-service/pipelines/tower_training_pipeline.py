fc8fcc68a25d838ae2b291b847191da13a773774
Михаил Карпенко <mkarpenko175@gmail.com>
2026-05-19 20:23:38 +0300

    add: added new pipelines for new equipment

--- data-service/pipelines/tower_training_pipeline.py
+++ data-service/pipelines/tower_training_pipeline.py
@@ -1,4 +1,4 @@
-# pipelines/vessel_training_pipeline.py
+# pipelines/utube_heat_training_pipeline.py
 import pandas as pd
 import logging
 from pathlib import Path
@@ -9,17 +9,16 @@ sys.path.append(str(BASE_DIR))
 
 from configs.config_loader import config
 from pipelines.base_etl import BaseETLPipeline
-from domain.vessel_features import VesselFeatureEngineer
+#from domain.vessel_features import VesselFeatureEngineer
 from utils.cleaners import vectorized_numeric_clean
-from utils.cleaners import clean_vessel_data
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-class VesselTrainingPipeline(BaseETLPipeline):
+class TowerTrainingPipeline(BaseETLPipeline):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.feature_engineer = VesselFeatureEngineer(self.config)
+#        self.feature_engineer = VesselFeatureEngineer(self.config)
 
     def transform(self, df_features: pd.DataFrame, df_weight: None) -> pd.DataFrame:
         raw = self.config['raw_names']
@@ -32,33 +31,30 @@ class VesselTrainingPipeline(BaseETLPipeline):
         # 2. переименование (сразу приводим к нормальным именам)
         df_merge = df_merge.rename(columns=self.get_rename_map())
 
+        # очистка от дубликатов
+        duplicated_cols = df_merge.columns[df_merge.columns.duplicated()].unique().tolist()
+        if duplicated_cols:
+            logger.warning(
+                f"ВНИМАНИЕ! Найдены дублирующиеся колонки: {duplicated_cols}. Берем только первые вхождения.")
+            df_merge = df_merge.loc[:, ~df_merge.columns.duplicated()]
+
         # 3. очистка чисел (теперь все вычисления будут безопасными)
         for col in self.config['cols_to_convert']:
             df_merge[col] = vectorized_numeric_clean(df_merge[col])
             df_merge[col] = pd.to_numeric(df_merge[col], errors='coerce')
 
-        # 4. физическая очистка от аномалий генератора
-        df_merge = clean_vessel_data(df_merge, cols)
-
-        # 5. валидация на NaN/Null
-        critical = [cols['diameter'], cols['ss_dist']]
-        df_merge = self.feature_engineer.filter_critical_data(df_merge, critical)
-
         logger.info("ФАКТИЧЕСКИЕ КОЛОНКИ: %s", df_merge.columns.tolist())
-
-        # 6. физика (feature engineering)
-        df_merge = self.feature_engineer.add_vessel_features(df_merge)
 
         return df_merge
 
     def load(self, df: pd.DataFrame):
-        super().load(df, filename='vessel_dataset_ml.csv')
+        super().load(df, filename='tower_dataset_ml.csv')
 
 if __name__ == '__main__':
-    vessel_ml_config = config['equipment']['vessel_ml']
-    pipeline = VesselTrainingPipeline(
-        input_file_path=BASE_DIR / 'data' / 'dataset_v_vessel_ml.xlsx',
+    tower_ml_config = config['equipment']['tower_ml']
+    pipeline = TowerTrainingPipeline(
+        input_file_path=BASE_DIR / 'data' / 'Trayed_tower.xlsx',
         output_folder_path=BASE_DIR / 'datasets',
-        config=vessel_ml_config
+        config=tower_ml_config
     )
     pipeline.run()
